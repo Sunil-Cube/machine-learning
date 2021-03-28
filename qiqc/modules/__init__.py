ecb7080d87862fbc4525890c07357da9c1a39aed
sunil <sunilsharma.sharma07@gmail.com>
2021-03-28 18:13:57 +0530

    add some  modules

--- qiqc/modules/__init__.py
+++ qiqc/modules/__init__.py
@@ -8,3 +8,11 @@ from qiqc.modules.classifier import BinaryClassifier
 
 from qiqc.modules.encoder.rnn import LSTMEncoder  # NOQA
 from qiqc.modules.encoder.rnn import LSTMGRUEncoder  # NOQA
+
+
+from qiqc.modules.aggregator.state import BiRNNLastStateAggregator
+from qiqc.modules.aggregator.pooling import MaxPoolingAggregator
+from qiqc.modules.aggregator.pooling import SumPoolingAggregator
+from qiqc.modules.aggregator.pooling import AvgPoolingAggregator
+
+from qiqc.modules.ensembler.simple import AverageEnsembler
